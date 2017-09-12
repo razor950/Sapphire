177a3d1b7f4b8d5833fa56109ef2dbd3502873c7
Mordred <ffxivsapphire@gmail.com>
2017-09-12 20:51:37 +0200

    Fixed mysql includes and more cmake cleanup

--- cmake/mysql.cmake
+++ cmake/mysql.cmake
@@ -2,15 +2,15 @@
 if(UNIX)
     include_directories("/usr/include/mysql/")
 else()
-    include_directories("${CMAKE_CURRENT_SOURCE_DIR}/../src/libraries/external/MySQL/")
+    include_directories("${LIBRARY_DIR}/external/MySQL/")
     if(CMAKE_SIZEOF_VOID_P EQUAL 4)
         # 32 bit link
-        link_directories(${CMAKE_CURRENT_SOURCE_DIR}/../src/libraries/external/MySQL/x86)
-        link_directories(${CMAKE_CURRENT_SOURCE_DIR}/../src/libraries/external/zlib/x86)
+        link_directories(${LIBRARY_DIR}/external/MySQL/x86)
+        link_directories(${LIBRARY_DIR}/external/zlib/x86)
     else()
         # 64 bit link
-        link_directories(${CMAKE_CURRENT_SOURCE_DIR}/../src/libraries/external/MySQL/x64)
-        link_directories(${CMAKE_CURRENT_SOURCE_DIR}/../src/libraries/external/zlib/x64)
+        link_directories(${LIBRARY_DIR}/external/MySQL/x64)
+        link_directories(${LIBRARY_DIR}/external/zlib/x64)
     endif()
 endif()
 
